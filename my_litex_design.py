6737f6a0b5c53456992f4e05a84c77267af3228d
Nick Østergaard <oe.nick@gmail.com>
2022-11-11 14:48:56 +0100

    Unfinished hack to try to sim a cpu in there

--- my_litex_design.py
+++ my_litex_design.py
@@ -1,17 +1,31 @@
 #!/usr/bin/env python3
 
-
+import argparse
 from litex.build.generic_platform import *
 from litex.build.sim import SimPlatform
 from litex.build.sim.config import SimConfig
+from litex.build.sim.verilator import verilator_build_args, verilator_build_argdict
 from litex.soc.cores.led import LedChaser
 from litex.soc.integration.builder import *
 # from amaranth.compat import *
 from migen import *
 
+from litex.soc.integration.soc_core import *
+from litex.soc.integration.soc import *
+from litex.soc.cores.cpu import CPUS
+
 _io = [
     ("sys_clk", 0, Pins(1)),
     ("sys_rst", 0, Pins(1)),
+    ("serial", 0,
+     Subsignal("source_valid", Pins(1)),
+     Subsignal("source_ready", Pins(1)),
+     Subsignal("source_data", Pins(8)),
+
+     Subsignal("sink_valid", Pins(1)),
+     Subsignal("sink_ready", Pins(1)),
+     Subsignal("sink_data", Pins(8)),
+     ),
     ("io_in", 0, Pins(8)),
     ("io_out", 0, Pins(8)),
 ]
@@ -24,6 +38,25 @@ class TinyTapeoutPlatform(SimPlatform):
     def __init__(self):
         SimPlatform.__init__(self, device="SIM", io=_io, name="sim")
 
+class SimSoC(SoCMini):
+    def __init__(self, **kwargs):
+        sys_clk_freq = int(1e6)
+        platform = TinyTapeoutPlatform()
+        # SoCCore ----------------------------------------------------------------------------------
+        SoCCore.__init__(self, platform, clk_freq=sys_clk_freq,
+            ident         = "LiteX Simulation  ____",
+            **kwargs)
+
+        # CRG --------------------------------------------------------------------------------------
+        #self.submodules.crg = CRG(platform.request("sys_clk"))
+
+        # Simulation platform ----------------------------------------------------------------------
+        # Only in SimPlatform, has to be enabled for tracing to run
+        if platform.device == "SIM":
+            self.comb += platform.trace.eq(1)
+
+        self.submodules.mymod = MyModule(platform, sys_clk_freq)
+        pass
 
 class MyModule(Module):
     def __init__(self, platform, sys_clk_freq):
@@ -60,6 +93,8 @@ class MyModule(Module):
         # Only in SimPlatform, has to be enabled for tracing to run
         if platform.device == "SIM":
             self.comb += platform.trace.eq(1)
+            pass
+
 
 
 def wokwi_module_name():
@@ -68,28 +103,68 @@ def wokwi_module_name():
     return f"user_module_{WOKWI_PROJECT_ID}"
 
 
+def sim_args(parser):
+    builder_args(parser)
+    soc_core_args(parser)
+    verilator_build_args(parser)
+
+
 def main():
     sys_clk_freq = int(1e6)
 
     # Verilog for tapeout ----------------------------------------------------------------------
-    non_sim_platform = MyPlatform()
-    non_sim_platform.name = wokwi_module_name()
-    my_mod = MyModule(non_sim_platform, sys_clk_freq)
-    v_output = non_sim_platform.get_verilog(fragment=my_mod, name=non_sim_platform.name)
-    v_output.write(f"src/{non_sim_platform.name}.v")
-    exit(0)
+    # non_sim_platform = MyPlatform()
+    # non_sim_platform.name = wokwi_module_name()
+    # my_mod = MyModule(non_sim_platform, sys_clk_freq)
+    # v_output = non_sim_platform.get_verilog(fragment=my_mod, name=non_sim_platform.name)
+    # v_output.write(f"src/{non_sim_platform.name}.v")
 
-    # Simulation platform ----------------------------------------------------------------------
-    platform = TinyTapeoutPlatform()
-    sim = MyModule(platform, sys_clk_freq)
+
+
+
+
+    # Configuration --------------------------------------------------------------------------------
+    parser = argparse.ArgumentParser(description="Generic LiteX SoC Simulation")
+    sim_args(parser)
+    args = parser.parse_args()
+
+    soc_kwargs = soc_core_argdict(args)
+    builder_kwargs = builder_argdict(args)
+    verilator_build_kwargs = verilator_build_argdict(args)
+
+
+    #cpu = CPUS.get(soc_kwargs.get("cpu_type", "vexriscv"))
+
     sim_config = SimConfig()
     sim_config.add_clocker("sys_clk", freq_hz=sys_clk_freq)
-    platform.build(sim, sim_config=sim_config, interactive=True, build_dir="./litex_out", run=True,
-                   trace=True,
-                   trace_fst=True,
-                   trace_start=0,
-                   trace_end=-1,
-                   )
+    # UART.
+    if soc_kwargs["uart_name"] == "serial":
+        soc_kwargs["uart_name"] = "sim"
+        sim_config.add_module("serial2console", "serial")
+
+    # SoC ------------------------------------------------------------------------------------------
+
+    soc = SimSoC(
+                 with_ethernet=False,
+                 with_etherbone=False,
+                 with_analyzer=False,
+                 with_i2c=False,
+                 with_sdcard=False,
+                 with_spi_flash=False,
+                 with_gpio=False,
+                 sim_debug=False,
+                 trace_reset_on=False,
+                 **soc_kwargs
+                 )
+
+    builder = Builder(soc, **builder_kwargs)
+    builder.build(
+        sim_config       = sim_config,
+        interactive=True,
+        run=True,
+        **verilator_build_kwargs,
+    )
+
 
 
 if __name__ == "__main__":
